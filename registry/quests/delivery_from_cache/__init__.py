5e20773ddcae9a580e06aa3e87c0992f14b04bec
menkent <unknown>
2017-07-19 11:25:13 +0300

    reg: DeliveryFromCache

--- registry/quests/delivery_from_cache/__init__.py
+++ registry/quests/delivery_from_cache/__init__.py
@@ -4,58 +4,94 @@ import logging
 log = logging.getLogger(__name__)
 
 from sublayers_server.model.registry_me.classes import notes
-from sublayers_server.model.registry_me.tree import RegistryLinkField, ListField, EmbeddedNodeField
+from sublayers_server.model.registry_me.tree import FloatField, EmbeddedDocumentField, ListField, EmbeddedNodeField
 from sublayers_server.model.quest_events import OnCancel, OnTimer, OnNote
 from sublayers_server.model.registry_me.classes.quests import (
-    Cancel,   QuestState_, FailByCancelState, FailState, WinState,
+    MarkerMapObject, Cancel, QuestState_, FailByCancelState, FailState, WinState,
 )
 
 import random
 from functools import partial
-
-from sublayers_server.model.poi_loot_objects import CreatePOICorpseEvent
+from sublayers_server.model.poi_loot_objects import CreatePOILootEvent, QuestPrivatePOILoot
 from sublayers_server.model.inventory import ItemState
+from sublayers_server.model.vectors import Point
 
-from sublayers_world.registry.quests.delivery_from_cache import DeliveryFromCache
+from sublayers_server.model.registry_me.classes.quests1 import DeliveryQuestSimple
 
 
-class SearchCourier(DeliveryFromCache):
-    courier_car_list = ListField(
-        caption=u"Список возможных машин курьера",
-        field=RegistryLinkField(
-            document_type='sublayers_server.model.registry_me.classes.mobiles.Car',
+class DeliveryFromCache(DeliveryQuestSimple):
+    cache_radius = FloatField(caption=u'Радиус, в котором можно обнаружить тайник', root_default=50)
+
+    cache_points_generator = ListField(
+        root_default=list,
+        caption=u"Список областей генерации мест для тайника",
+        field=EmbeddedDocumentField(document_type=MarkerMapObject),
+        reinst=False,
+    )
+    cache_point = EmbeddedDocumentField(document_type=MarkerMapObject, reinst=False)
+
+    loot_set_list = ListField(
+        root_default=list,
+        caption=u"Список возможных комплектов ненужных вещей",
+        field=ListField(
+            caption=u"Комплект ненужных вещей",
+            field=EmbeddedNodeField(
+                document_type='sublayers_server.model.registry_me.classes.item.Item',
+                caption=u"Необходимый итем",
+            ),
         ),
     )
-    courier_car = EmbeddedNodeField(
-        document_type='sublayers_server.model.registry_me.classes.mobiles.Car',
-        caption=u"Машина курьера",
+    loot_set = ListField(
+        caption=u"Список ненужных вещей",
+        field=EmbeddedNodeField(
+            document_type='sublayers_server.model.registry_me.classes.item.Item',
+            caption=u"Необходимый итем",
+        ),
     )
 
-    courier_medallion = EmbeddedNodeField(
-        document_type='sublayers_server.model.registry_me.classes.quest_item.QuestItem',
-        caption=u"Медальон курьера",
-        tags={'client'},
-    )
+    def init_level(self):
+        self.level = 1
+
+    def generate_reward(self):
+        self.reward_money = 0
+        self.reward_karma = 2
+        self.reward_relation_hirer = 5
 
     def init_delivery_set(self):
+        # Тут гененрация посылок
         self.delivery_set = []
+        choice = random.choice(self.delivery_set_list[0])
+        item = choice.instantiate(amount=choice.amount)
+        self.delivery_set.append(item)
 
         # Тут гененрация ненужных вещей
-        loot_set = []
-        for i in range(random.choice([3, 4])):  # 3-4 предмета
-            # Выбор только по первому элементу списка (т.к. в простой реализации квеста есть только 1 список итемов, а не пресеты)
+        self.loot_set = []
+        for i in range(random.choice([3, 4])): # 3-4 предмета
             choice = random.choice(self.loot_set_list[0])
             item = choice.instantiate(amount=choice.amount)
-            loot_set.append(item)
-        self.loot_set = loot_set
+            self.loot_set.append(item)
 
-        # Выбор машинки курьера
-        choice = random.choice(self.courier_car_list)
-        self.courier_car = choice.instantiate()
+    def init_target_point(self):
+        base_point = random.choice(self.cache_points_generator)
+        self.cache_point = MarkerMapObject(position=base_point.generate_random_point(), radius=self.cache_radius)
+
+    def init_distance(self):
+        p1 = self.hirer.hometown.position.as_point()
+        p2 = self.cache_point.position.as_point()
+        return p1.distance(p2) * 2  # дистация двойная, так как нужно съездить туда и обратно
+
+    def init_deadline(self, distance):
+        # Время выделенное на квест в секундах
+        if self.design_speed:
+            all_time = int(distance / self.design_speed)
+            # Время выделенное на квест кратно 5 минутам
+            self.deadline = (all_time / 300) * 300 + (300 if (all_time % 300) > 0 else 0)
+        else:
+            self.deadline = 0
 
     def init_text(self):
-        self.text_short = u"Найти пропавшего курьера."
-        self.text = u"Найти пропавшего курьера и вернуть важный предмет{} Награда: {:.0f}nc, {:.0f} кармы и {:.0f} ед. опыта.".format(
+        self.text_short = u"Найти пропавшую посылку."
+        self.text = u"Вернуть пропавшую посылку.{} Награда: {:.0f}nc, {:.0f} кармы и {:.0f}ед. опыта.".format(
             u"." if not self.deadline else u" за {}.".format(self.deadline_to_str()),
             self.reward_money,
             self.reward_karma,
@@ -67,59 +103,71 @@ class SearchCourier(DeliveryFromCache):
             life_time = self.starttime + self.deadline - event.time
         else:
             life_time = event.server.poi_loot_objects_life_time
+        private_name = self.agent.profile._agent_model and self.agent.profile._agent_model.print_login() or self.agent.login
 
         items = []
+        item = self.delivery_set[0].instantiate(amount=self.delivery_set[0].amount)
+        items.append(ItemState(server=event.server, time=event.time, example=item, count=item.amount))
+        self.dc.package_uid = item.uid
         for item_example in self.loot_set:
             item = item_example.instantiate(amount=item_example.amount)
             items.append(ItemState(server=event.server, time=event.time, example=item, count=item.amount))
 
-        medallion = self.courier_medallion.instantiate()
-        self.dc.medallion_uid = medallion.uid
-        self.agent.profile.quest_inventory.add_item(agent=self.agent, item=medallion, event=event)
-        self.log(text=u'Получена платиновая фишка. Вернитесь в город для завершения квеста.', event=event, position=self.cache_point.position)
-
-        CreatePOICorpseEvent(
+        CreatePOILootEvent(
             server=event.server,
             time=event.time,
+            poi_cls=QuestPrivatePOILoot,
             example=None,
-            inventory_size=len(self.loot_set),
-            position=self.cache_point.position.as_point(),
+            inventory_size=len(self.delivery_set) + len(self.loot_set),
+            position=Point.random_gauss(self.cache_point.position.as_point(), self.cache_point.radius),
             life_time=life_time,
             items=items,
-            sub_class_car=self.courier_car.sub_class_car,
-            car_direction=0,
-            donor_v=0,
-            donor_example=self.courier_car,
-            agent_viewer=None,
+            connect_radius=0,
+            extra=dict(private_name=private_name),
         ).post()
 
-    def take_medallion(self, event):
-        medallion = self.agent.profile.quest_inventory.get_item_by_uid(self.dc.medallion_uid)
-        # todo: medallion не может не быть, если его нет то хз вообще как
-        if medallion:
-            self.agent.profile.quest_inventory.del_item(agent=self.agent, item=medallion, event=event)
+    def can_take_package(self, event):
+        if not self.agent.profile.car:
+            return False
+        if self.agent.profile._agent_model:
+            self.agent.profile._agent_model.inventory.save_to_example(time=event.time)
+        return self.agent.profile.car.inventory.get_item_by_uid(uid=self.dc.package_uid) is not None
+
+    def take_items_package(self, event):
+        if not self.can_take_package(event=event):
+            return False
+        item = self.agent.profile.car.inventory.get_item_by_uid(uid=self.dc.package_uid)
+        self.agent.profile.car.inventory.items.remove(item)
+        if self.agent.profile._agent_model:
+            self.agent.profile._agent_model.reload_inventory(time=event.time, save=False)
         return True
+
+    def as_client_dict(self):
+        d = super(DeliveryFromCache, self).as_client_dict()
+        d.update(
+            package_example=self.delivery_set[0].as_client_dict() if self.delivery_set and len(self.delivery_set) > 0 else None,
+            package_uid=getattr(self.dc, 'package_uid', None)
+        )
+        return d
 
     ####################################################################################################################
     def on_generate_(self, event, **kw):
         if not self.can_generate(event):
             raise Cancel("QUEST CANCEL: reason: generate rules")
 
+        if not self.delivery_set_list:
+            raise Cancel("QUEST DeliveryFromCache CANCEL: Empty empty delivery_set_list.")
         if self.hirer.hometown is None:
-            raise Cancel("QUEST SearchCourier CANCEL: {} hometown is None.".format(self.hirer.hometown))
-        if not self.courier_car_list:
-            raise Cancel("QUEST SearchCourier CANCEL: Empty courier_car_list.")
+            raise Cancel("QUEST DeliveryFromCache CANCEL: {} hometown is None.".format(self.hirer.hometown))
 
         self.init_level()
         self.init_delivery_set()
         self.init_target_point()
 
         distance = self.init_distance()
-        if distance == 0:
-            pass
-            # todo: настроить логирование в квестах
-            # log('SearchCourier Quest: Warning!!! Distance from hirer<{}> to point<{}> = {}'.format(self.hirer, self.cache_point, distance))
-        self.init_deadline(distance=distance)
+        # if distance == 0:
+        #     log('DeliveryFromCache Quest: Warning!!! Distance from hirer<{}> to point<{}> = {}'.format(self.hirer, self.cache_point, distance))
+        self.init_deadline(distance)
 
         self.generate_reward()  # Устанавливаем награду за квест (карму, деньги и итемы)
         self.init_text()  # Инициализируем строку описания квеста
@@ -128,8 +176,8 @@ class SearchCourier(DeliveryFromCache):
     def on_start_(self, event, **kw):
         if self.get_available_lvl() < self.level:
             self.npc_replica(npc=self.hirer, replica=u"NPC не достаточно хорошо к Вам относится.", event=event)
-            raise Cancel("QUEST SearchCourier CANCEL: User have not enough relation")
-        self.log(text=u'Начат квест по поиску пропавшего курьера.', event=event, position=self.hirer.hometown.position)
+            raise Cancel("QUEST DeliveryFromCache CANCEL: User have not enough relation")
+        self.log(text=u'Начат квест по поиску посылки.', event=event, position=self.hirer.hometown.position)
 
     ####################################################################################################################
     ## Перечень состояний ##############################################################################################
@@ -172,13 +220,13 @@ class SearchCourier(DeliveryFromCache):
     class cache(QuestState_):
         def on_enter_(self, quest, event):
             # создать лут с временем жизни до окончания дедлайна и с нужными итемами
-            quest.log(text=u'Найдена машина курьера.', event=event, position=quest.cache_point.position)
             quest.create_poi_container(event)
+            quest.log(text=u'Найдена посылка.', event=event, position=quest.cache_point.position)
 
             # создать ноту на доставку
             quest.dc.delivery_note_uid = quest.agent.profile.add_note(
                 quest_uid=quest.uid,
-                note_class=notes.NPCDeliveryNoteCourier,
+                note_class=notes.NPCDeliveryNotePackage,
                 time=event.time,
                 npc=quest.hirer,
                 page_caption=quest.caption,
@@ -189,15 +237,15 @@ class SearchCourier(DeliveryFromCache):
             go = partial(quest.go, event=event)
 
             if isinstance(event, OnCancel):
-                quest.npc_replica(npc=quest.hirer, replica=u"Вы нашли машину курьера и не можете отказаться.", event=event)
+                quest.npc_replica(npc=quest.hirer, replica=u"Вы нашли посылку и не можете отказаться.", event=event)
 
             if isinstance(event, OnTimer) and event.name == 'deadline_delivery_cache_quest':
                 agent.profile.del_note(uid=quest.dc.delivery_note_uid, time=event.time)
                 go("fail")
 
             if isinstance(event, OnNote):
-                if (event.note_uid == quest.dc.delivery_note_uid) and (event.result == True) and quest.take_medallion(
-                        event=event):
+                if ((event.note_uid == quest.dc.delivery_note_uid) and (event.result == True) and
+                        quest.take_items_package(event=event)):
                     agent.profile.del_note(uid=quest.dc.delivery_note_uid, time=event.time)
                     go('reward')
 
@@ -227,4 +275,5 @@ class SearchCourier(DeliveryFromCache):
             quest.agent.profile.set_relationship(time=event.time, npc=quest.hirer, dvalue=-20)  # изменение отношения c нпц
             quest.agent.profile.set_karma(time=event.time, dvalue=-10)  # изменение кармы
             quest.log(text=u'Квест провален.', event=event)
+
 
