c9b861c9de1e0c4869a109516f4f40d480ce7e78
menkent <unknown>
2017-08-09 12:06:22 +0300

    reg: добавлена рандомизация базовой цены машинок мобов

--- registry/quests/ai_event_quests/traffic/gang/__init__.py
+++ registry/quests/ai_event_quests/traffic/gang/__init__.py
@@ -76,6 +76,7 @@ class AIGangQuest(AITrafficQuest):
                             car_params=dict(
                                 position=car_pos,
                                 direction=random.random() * 2 * pi,
+                                base_exp_price=self.bots_car_exp.get_random_int(),
                             )
                         )
                     timer_summ_car_ex += deploy_timer_summ_car_ex.duration
