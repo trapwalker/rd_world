12cc9753f54bdddb3f7a0657cc10501851480c32
menkent <unknown>
2017-08-11 15:24:10 +0300

    reg: quests: caravan max party capacity + Exclude on OnCancel Quest

--- registry/quests/agent_event_quests/escort_caravan/__init__.py
+++ registry/quests/agent_event_quests/escort_caravan/__init__.py
@@ -5,6 +5,7 @@ log = logging.getLogger(__name__)
 from sublayers_world.registry.quests.agent_event_quests import AgentEventQuest
 from sublayers_world.registry.quests.ai_event_quests.traffic.gang.caravan_simple import AICaravanQuest
 from sublayers_server.model.quest_events import OnTimer, OnPartyExclude
+from sublayers_server.model.registry_me.classes.quests import Cancel
 
 from functools import partial
 
@@ -57,6 +58,12 @@ class EscortCaravan(AgentEventQuest):
         self.dc.check_participation = 0.0
         self.dc.count_participation = 0.0
         self.dc.caravan_started = False
+        event_quest = self.get_event_quest(event=event)
+        if event_quest and self.agent.profile._agent_model:
+            res = event_quest.include_to_party(model_agent=self.agent.profile._agent_model, event=event)
+            if not res:
+                self.npc_replica(npc=self.hirer, replica=u"Караван сформирован.", event=event)
+                raise Cancel("QUEST CANCEL: Caravan Party is full.")
 
     ####################################################################################################################
     class begin(AgentEventQuest.begin):
@@ -64,9 +71,7 @@ class EscortCaravan(AgentEventQuest):
             super(EscortCaravan.begin, self).on_enter_(quest=quest, event=event)
             quest.set_timer(event=event, name='participation', delay=30)
             quest.log(u'Ожидание каравана.', event=event)
-            event_quest = quest.get_event_quest(event=event)
-            if event_quest and quest.agent.profile._agent_model:
-                event_quest.include_to_party(model_agent=quest.agent.profile._agent_model, event=event)
+
 
         def on_event_(self, quest, event):
             super(EscortCaravan.begin, self).on_event_(quest=quest, event=event)
@@ -88,6 +93,9 @@ class EscortCaravan(AgentEventQuest):
             if isinstance(event, OnPartyExclude) and event.agent and event.agent is quest.agent.profile._agent_model:
                 # Отказ от квеста (будто у нпц отказался)
                 quest.go(new_state='cancel_fail', event=event)
+                event_quest = quest.get_event_quest(event=event)
+                if event_quest:
+                    event_quest.exclude_from_party(model_agent=event.agent, event=event)
 
     ####################################################################################################################
     class win(AgentEventQuest.win):
@@ -102,4 +110,10 @@ class EscortCaravan(AgentEventQuest):
     class cancel_fail(AgentEventQuest.cancel_fail):
         def on_enter_(self, quest, event):
             super(EscortCaravan.cancel_fail, self).on_enter_(quest=quest, event=event)
+
+            event_quest = quest.get_event_quest(event=event)
+            if event_quest and event_quest.dc.party is not None and quest.agent.profile._agent_model in event_quest.dc.party:
+                event_quest.dc.party.on_exclude(agent=quest.agent.profile._agent_model, time=event.time)
+                event_quest.exclude_from_party(model_agent=quest.agent.profile._agent_model, event=event)
+
             quest.log(u'Отказ от участия в караване.', event=event)
