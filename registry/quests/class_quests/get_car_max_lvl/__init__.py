1c24fa34d0a32edce817369a52fbbe9c0050a3f3
menkent <unknown>
2017-10-31 14:48:35 +0300

    reg: GetCarMaxLevelQuest

---
+++ registry/quests/class_quests/get_car_max_lvl/__init__.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import logging
+log = logging.getLogger(__name__)
+
+from sublayers_server.model.registry_me.classes.quests import QuestState_, WinState
+from sublayers_server.model.quest_events import OnNote
+from sublayers_server.model.registry_me.classes import notes
+from functools import partial
+from sublayers_server.model.registry_me.tree import LocalizedString
+
+from sublayers_world.registry.quests.class_quests import ClassTypeQuest
+
+
+class GetCarMaxLevelQuest(ClassTypeQuest):
+    def init_text(self):
+        # todo: ##LOCALIZATION
+        self.text = LocalizedString(
+            en=u"Adventure will begin soon.",   # todo: ##LOCALIZATION
+            ru=u"Приключение начнется скоро.",
+        )
+
+    def on_start_(self, event, **kw):
+        self.init_text()
+        self.log(text=self.locale("q_cq_get_car_lvl_started"), event=event)  ##LOCALIZATION
+
+
+    ####################################################################################################################
+    class begin(QuestState_):
+        def on_enter_(self, quest, event):
+            quest.dc.car_info_note = quest.agent.profile.add_note(
+                quest_uid=quest.uid,
+                note_class=notes.GetClassCarQuestNote,
+                time=event.time,
+                page_caption=quest.locale("q_cq_get_car_lvl_note_page"),  ##LOCALIZATION
+                btn1_caption=quest.locale("q_cq_get_car_lvl_note_btn"),  ##LOCALIZATION
+                npc=quest.hirer,
+            )
+
+        def on_event_(self, quest, event):
+            if isinstance(event, OnNote) and (event.note_uid == quest.dc.car_info_note):
+                agent = quest.agent.profile
+                if agent.car and agent.car.get_real_lvl() > 4: # info по идее это максимальный уровень машинки
+                    agent.del_note(uid=quest.dc.car_info_note, time=event.time)
+                    quest.go(event=event, new_state="win")
+                else:
+                    quest.npc_replica(
+                        npc=quest.hirer,
+                        replica=quest.locale("q_cq_get_car_lvl_bad"),  ##LOCALIZATION
+                        event=event
+                    )
+
+
+    ####################################################################################################################
+    class win(WinState):
+        def on_enter_(self, quest, event):
+            quest.log(text=quest.locale("q_cq_get_car_lvl_finished"), event=event)  # ##LOCALIZATION
+            agent_example = quest.agent
+            new_quest = quest.next_quest.instantiate(abstract=False, hirer=quest.hirer)
+            if new_quest.generate(event=event, agent=agent_example):
+                agent_example.profile.add_quest(quest=new_quest, time=event.time)
+                new_quest.start(server=event.server, time=event.time + 0.1)
+            else:
+                del new_quest
